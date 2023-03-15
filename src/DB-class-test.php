8435e32167a9d363fb014778ca27482615cd5e3c
Cyril Cornez <77747225+CCornez@users.noreply.github.com>
2023-03-15 22:59:36 +0100

    Update test file: require -> require_once

--- src/DB-class-test.php
+++ src/DB-class-test.php
@@ -1,7 +1,7 @@
 <?php
 
-require "includes/Db.class.php";
-require "includes/Todos.class.php";
+require_once "includes/Db.class.php";
+require_once "includes/Todos.class.php";
 
 $db = new Db();
 $todos = new Todos($db);
